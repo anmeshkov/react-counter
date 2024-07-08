431ec1a5c57ebb61046ca9a8f30af0173aaf2207
Andrew Meshkov <meshkov89@gmail.com>
2024-07-08 22:00:18 +0300

    refactor: рефакторинг разметки кнопок

--- src/components/ButtonsGroup.js
+++ src/components/ButtonsGroup.js
@@ -9,32 +9,27 @@ class BtnGroup extends Component {
   };
 
   render() {
+    const buttons = [
+      { id: "btnMinus", name: "Minus" },
+      { id: "btnReset", name: "Reset" },
+      { id: "btnPlus", name: "Plus" },
+    ];
+
+    const renderButtons = buttons.map((btn) => {
+      return (
+        <button
+          onClick={this.onButtonClick}
+          type="button"
+          className="btn btn-outline-primary"
+          id={btn.id}
+        >
+          {btn.name}
+        </button>
+      );
+    });
     return (
       <div className="btn-group" role="group" aria-label="Basic example">
-        <button
-          onClick={this.onButtonClick}
-          type="button"
-          className="btn btn-outline-primary"
-          id="btnMinus"
-        >
-          Minus
-        </button>
-        <button
-          onClick={this.onButtonClick}
-          type="button"
-          className="btn btn-outline-primary"
-          id="btnReset"
-        >
-          Reset
-        </button>
-        <button
-          onClick={this.onButtonClick}
-          type="button"
-          className="btn btn-outline-primary"
-          id="btnPlus"
-        >
-          Plus
-        </button>
+        {renderButtons}
       </div>
     );
   }
